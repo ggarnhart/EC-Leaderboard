a5011a142f1ecdc2de6515ee3664ccffe6a56c01
Greg Garnhart <gegarnhart@email.wm.edu>
2019-01-06 11:33:45 -0500

    Updates

---
+++ precache-manifest.243c7b80a43d7a4d4bc0c0c584ed3eb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EC-Leaderboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5a6ece89072a886d7bb9",
+    "url": "/EC-Leaderboard/static/js/runtime~main.5a6ece89.js"
+  },
+  {
+    "revision": "ac474cc5e66d24a3ff2f",
+    "url": "/EC-Leaderboard/static/js/main.ac474cc5.chunk.js"
+  },
+  {
+    "revision": "febc73ff26c7d8f4e516",
+    "url": "/EC-Leaderboard/static/js/1.febc73ff.chunk.js"
+  },
+  {
+    "revision": "ac474cc5e66d24a3ff2f",
+    "url": "/EC-Leaderboard/static/css/main.97bb0855.chunk.css"
+  },
+  {
+    "revision": "febc73ff26c7d8f4e516",
+    "url": "/EC-Leaderboard/static/css/1.fa616e99.chunk.css"
+  },
+  {
+    "revision": "cc74d93f7beaaff49ece0527cacca7d6",
+    "url": "/EC-Leaderboard/index.html"
+  }
+];
